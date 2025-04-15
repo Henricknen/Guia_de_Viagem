e1ae58d1f2ad0a9d5f7407ee0161d85ddb20209c
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-04-14 22:05:18 -0300

    configurando_'provider'

--- flutter_out_guide/lib/src/models/appdata.dart
+++ flutter_out_guide/lib/src/models/appdata.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppData with ChangeNotifier {
     var data = [];      // array 'data' que conterá as informações
 
-    void setData(newData) {     // 'setData' reçebe 'newData' os novos dados e tem a possibilidade de alterar esse novos dados reçebidos
+    void setData(newData) {     // 'setData' reçebe 'newData' os novos dados e tem a possibilidade de alterar esse novos dados recebidos
         data = newData;
         notifyListeners();
     }
