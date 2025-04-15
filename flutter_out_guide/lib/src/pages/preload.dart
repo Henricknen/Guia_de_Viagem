87785d3efc7f2ed910625481ae7b0435c5f64bbf
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-04-15 20:06:28 -0300

    inserindo_'imagem_texto_botao'_e_criando_condicao_para_aparecer_que_os_mesmos_aparecam_se_'loading'_estiver_'true'_o_'false'

--- flutter_out_guide/lib/src/pages/preload.dart
+++ flutter_out_guide/lib/src/pages/preload.dart
@@ -9,18 +9,40 @@ class _PreloadPage extends State<PreloadPage> {
     bool loading = false;
 
     @override
-    Widget build(BuildContext contex) {
+    Widget build(BuildContext context) {
         return Scaffold(
             body:Center(
                 child: Column(
-                    mainAxisAlingment: MainAxisAlingment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                         Image.asset('lib/assets/mundo.jfif',
                             width: 200,
-                        )
+                        ),
+
+                        loading ? Container(        // condiçional se 'loading' estiver 'true' esse container aparecerá
+                            margin: EdgeInsets.all(30),
+                            child: Text('Carregando informações...',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                ),
+                            ),
+                        ) : Container(),        // se 'loading' for false aparecerá o container vazio
+
+                        loading ? CircularProgressIndicator(
+                            strokeWidth: 3,
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                        ) : Container(),
+                        
+                        !loading ? Container(       // se 'loading' estiver false mostra o botão
+                            margin: EdgeInsets.all(30),
+                            child: ElevatedButton(
+                                child: Text('Tentar novamente'),
+                                onPressed: (){},
+                            )
+                        ) : Container(),
                     ],
                 )
             )
         );
     }
-}
+} 
