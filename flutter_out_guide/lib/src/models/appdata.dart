13738a3c1c3836cb479d75de817691be2fe3524d
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-04-16 22:37:23 -0300

    fazendo_'requizicao'_de_uma_api

--- flutter_out_guide/lib/src/models/appdata.dart
+++ flutter_out_guide/lib/src/models/appdata.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;        // puxando o pacote http
 
 class AppData with ChangeNotifier {
     var data = [];      // array 'data' que conterá as informações
@@ -6,5 +7,18 @@ class AppData with ChangeNotifier {
     void setData(newData) {     // 'setData' reçebe 'newData' os novos dados e tem a possibilidade de alterar esse novos dados recebidos
         data = newData;
         notifyListeners();
+    }
+
+    Future<bool> requestData() async{      // função assicrona 
+        final res = await http.get(Uri.parse('https://restcountries.com/v3.1/all/'));      // fazendo 'requizição' da api
+
+        if(res.statusCode == 200) {
+            // inserção dos dados em DATA
+            print( res.body );
+            // setData( res.body );
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- flutter_out_guide/lib/src/pages/preload.dart
+++ flutter_out_guide/lib/src/pages/preload.dart
@@ -1,51 +1,87 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';         // importando pacote 'services'
+    import 'package:flutter/material.dart';
+    import 'package:flutter/services.dart'; // importando pacote 'services'
+    import 'package:provider/provider.dart';
+    import '../models/appdata.dart';
 
-class PreloadPage extends StatefulWidget {
+    class PreloadPage extends StatefulWidget {
     @override
     _PreloadPage createState() => _PreloadPage();
-}
+    }
 
-class _PreloadPage extends State<PreloadPage> {
-    bool loading = false;
+    class _PreloadPage extends State<PreloadPage> {
+    bool loading = true;
+
+    void requestInfo() async {
+        await Future.delayed(Duration(seconds: 1)); // retardando a requisição por 1 segundo
+
+        setState(() {
+        loading = true;
+        });
+
+        bool req = await Provider.of<AppData>(context, listen: false).requestData();
+
+        if (req) {
+        // ir para home
+        print("Deu certo...");
+        } else {
+        print("Não deu certo...");
+        }
+
+        setState(() {
+        loading = false;
+        });
+    }
+
+    @override
+    void initState() {
+        super.initState();
+        requestInfo();
+    }
 
     @override
     Widget build(BuildContext context) {
         SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
-        return Scaffold(
-            body:Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Image.asset('lib/assets/mundo.jfif',
-                            width: 200,
+            return Scaffold(
+        body: Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+                Image.asset(
+                'lib/assets/mundo.jfif',
+                width: 200,
+                ),
+                loading
+                    ? Container(
+                        margin: EdgeInsets.all(30),
+                        child: Text(
+                        'Carregando informações...',
+                        style: TextStyle(
+                            fontSize: 16,
                         ),
-
-                        loading ? Container(        // condiçional se 'loading' estiver 'true' esse container aparecerá
-                            margin: EdgeInsets.all(30),
-                            child: Text('Carregando informações...',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                ),
-                            ),
-                        ) : Container(),        // se 'loading' for false aparecerá o container vazio
-
-                        loading ? CircularProgressIndicator(
-                            strokeWidth: 3,
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                        ) : Container(),
-                        
-                        !loading ? Container(       // se 'loading' estiver false mostra o botão
-                            margin: EdgeInsets.all(30),
-                            child: ElevatedButton(
-                                child: Text('Tentar novamente'),
-                                onPressed: (){},
-                            )
-                        ) : Container(),
-                    ],
-                )
-            )
+                        ),
+                    )
+                    : Container(),
+                loading
+                    ? CircularProgressIndicator(
+                        strokeWidth: 3,
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                    )
+                    : Container(),
+                !loading
+                    ? Container(
+                        margin: EdgeInsets.all(30),
+                        child: ElevatedButton(
+                        child: Text('Tentar novamente'),
+                        onPressed: () {
+                            requestInfo();
+                        },
+                        ),
+                    )
+                    : Container(),
+            ],
+            ),
+        ),
         );
     }
-} 
+    }
