85e922aa21d0ff77ecbe84200f0753f2f5402c2b
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-04-19 20:05:04 -0300

    inciciando_correcao_no_codigo

--- flutter_out_guide/lib/src/models/appdata.dart
+++ flutter_out_guide/lib/src/models/appdata.dart
@@ -1,22 +1,22 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;        // puxando o pacote http
 
 class AppData with ChangeNotifier {
     var data = [];      // array 'data' que conterá as informações
 
-    void setData(newData) {     // 'setData' reçebe 'newData' os novos dados e tem a possibilidade de alterar esse novos dados recebidos
+    void setData(newData) {     // 'setData' reçebe 'newData' os novos dados, e tem a possibilidade de alterar esse novos dados recebidos
         data = newData;
         notifyListeners();
     }
 
-    Future<bool> requestData() async{      // função assicrona 
-        final res = await http.get(Uri.parse('https://restcountries.com/v3.1/all/'));      // fazendo 'requizição' da api
+    Future<bool> requestData() async {      // função assicrona pega os dados do 'webservice' api
+        final uri = Uri.parse('https://api.b7web.com.br/flutter1wb/');  // converte a URL para o tipo 'Uri'
+        final res = await http.get(uri);  // agora o parâmetro é um 'Uri'
 
-        if(res.statusCode == 200) {
-            // inserção dos dados em DATA
-            print( res.body );
-            // setData( res.body );
-            return true;
+        if(res.statusCode == 200) {     // se der uma respostas satisfatória
+            setData( jsonDecode(res.body) );        // inseri o dados em em 'setData'
+            return true;        // e retona 'true'
         } else {
             return false;
         }
--- flutter_out_guide/lib/src/pages/preload.dart
+++ flutter_out_guide/lib/src/pages/preload.dart
@@ -1,40 +1,39 @@
-    import 'package:flutter/material.dart';
-    import 'package:flutter/services.dart'; // importando pacote 'services'
-    import 'package:provider/provider.dart';
-    import '../models/appdata.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // importando pacote 'services'
+import 'package:provider/provider.dart';
+import '../models/appdata.dart';
 
-    class PreloadPage extends StatefulWidget {
-    @override
-    _PreloadPage createState() => _PreloadPage();
-    }
+class PreloadPage extends StatefulWidget {
+@override
+_PreloadPage createState() => _PreloadPage();
+}
 
-    class _PreloadPage extends State<PreloadPage> {
+class _PreloadPage extends State<PreloadPage> {
     bool loading = true;
 
-    void requestInfo() async {
+    void requestInfo() async {      // função que fará a requisição
         await Future.delayed(Duration(seconds: 1)); // retardando a requisição por 1 segundo
 
         setState(() {
-        loading = true;
+            loading = true;
         });
 
-        bool req = await Provider.of<AppData>(context, listen: false).requestData();
+        bool req = await Provider.of<AppData>(context).requestData();
 
         if (req) {
-        // ir para home
-        print("Deu certo...");
+            Navigator.pushReplacementNamed(context, '/home');
         } else {
-        print("Não deu certo...");
+            setState(() {
+                loading = false;
+            });
         }
 
-        setState(() {
-        loading = false;
-        });
     }
 
-    @override
+    // @override
     void initState() {
         super.initState();
+
         requestInfo();
     }
 
@@ -42,46 +41,41 @@
     Widget build(BuildContext context) {
         SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
-            return Scaffold(
-        body: Center(
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                Image.asset(
-                'lib/assets/mundo.jfif',
-                width: 200,
+        return Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        Image.asset('lib/assets/mundo.jfif',
+                            width: 200,                
+                        ),
+
+                        loading ? Container(        // se 'loading' for 'true' aparecerá as informações abaixo
+                            margin: EdgeInsets.all(30),
+                            child: Text('Carregando informações...',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                ),
+                            ),
+                        ) : Container(),        // se for 'false' aparecerá um container vazio ou seja não irá aparecer nada
+                        
+                        loading ? CircularProgressIndicator(
+                            strokeWidth: 3,
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                        ) : Container(),
+                        
+                        !loading ? Container(
+                            margin: EdgeInsets.all(30),
+                            child: ElevatedButton(
+                            child: Text('Tentar novamente'),
+                            onPressed: () {
+                                requestInfo();
+                            },
+                            )
+                        ) : Container(),
+                    ],
                 ),
-                loading
-                    ? Container(
-                        margin: EdgeInsets.all(30),
-                        child: Text(
-                        'Carregando informações...',
-                        style: TextStyle(
-                            fontSize: 16,
-                        ),
-                        ),
-                    )
-                    : Container(),
-                loading
-                    ? CircularProgressIndicator(
-                        strokeWidth: 3,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                    )
-                    : Container(),
-                !loading
-                    ? Container(
-                        margin: EdgeInsets.all(30),
-                        child: ElevatedButton(
-                        child: Text('Tentar novamente'),
-                        onPressed: () {
-                            requestInfo();
-                        },
-                        ),
-                    )
-                    : Container(),
-            ],
             ),
-        ),
         );
     }
-    }
+}
